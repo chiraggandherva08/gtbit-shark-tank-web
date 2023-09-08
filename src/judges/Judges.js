0cb634a14f33a29006f25d7ccd9567e2dc5e4832
Chirag Gandherva <chiraggandherva08@gmail.com>
2023-09-08 16:58:45 +0530

    modified file

--- src/judges/Judges.js
+++ src/judges/Judges.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Judges = () => {
     return <React.Fragment>
+        <div id="judges"></div>
         <h1 id="team-heading">Our Judges</h1>
         <ul id="judges-list">
             <li className="team-member">
@@ -17,7 +18,7 @@ const Judges = () => {
                 </div>
             </li>
             <li className="team-member">
-                <img className="team-member-pic" src={`assets/team-pics/${"Deepali"}`} alt="team-memb"/>
+                <img className="team-member-pic-user" src={`assets/team-pics/${"user.svg"}`} alt="team-memb"/>
                 <div className="team-member-name">
                     Dr. Amandeep Kaur
                 </div>
